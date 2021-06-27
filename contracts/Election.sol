71e17bff099fa8cd9feff0f69a4e747d7a6e928d
Uttam Singh <uttamkhanduja@yahoo.in>
2021-06-27 22:30:13 +0530

    Updated contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,26 @@
 pragma solidity ^0.5.16;
 
 contract Election{
-    // store candidate
-    // Read candidate
-    string public candidate;
-    // Constructor
-    constructor() public{
-        candidate= "Candidate 1";
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
 
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    // // Constructor
+    constructor() public{
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 }
